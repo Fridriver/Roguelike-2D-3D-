53f56c7dbb2252d25517a2b7f4bd10beaf5018ec
Fridriver <117280045+Fridriver@users.noreply.github.com>
2024-01-20 12:05:33 -0500

    Système de déplacement (Manche) Pas encore fini mais fonctionnel

--- Assets/Scripts/Vaisseau.cs
+++ Assets/Scripts/Vaisseau.cs
@@ -7,6 +7,8 @@ public class Vaisseau : MonoBehaviour
     [SerializeField] private float vitesseDeplacement = 5f;
     [SerializeField] private float vitesseRotation = 5f;
 
+    [SerializeField] private GameObject mancheRotation;
+
     private Rigidbody rb;
 
     private void Start()
@@ -25,7 +27,8 @@ public class Vaisseau : MonoBehaviour
             float rotationY = Input.GetAxis("Mouse X") * vitesseRotation * Time.deltaTime;
 
             // Appliquer la rotation au vaisseau
-            transform.Rotate(rotationX, rotationY, 0f);
+            transform.Rotate(-rotationX, rotationY, 0f);
+            mancheRotation.transform.Rotate(-rotationX, 0f, -rotationY);
 
             // Déplacer le vaisseau vers le haut ou le bas en fonction du mouvement vertical de la souris
             float mouvementVerticalSouris = Input.GetAxis("Mouse Y");
@@ -34,8 +37,20 @@ public class Vaisseau : MonoBehaviour
         }
         else
         {
-            
+            // Faire en sorte que les rotations x, y, et z du mancheRotation reviennent à 0
+
+            // Obtenez la rotation actuelle du mancheRotation
+            Quaternion rotationActuelle = mancheRotation.transform.rotation;
+            Vector3 rotationVaisseauActuelle = transform.rotation.eulerAngles;
+
+            // Créez une nouvelle rotation avec chacune des composantes (x, y, z) à 0
+            Quaternion rotationCible = Quaternion.Euler(0f, 0f, 0f);
+
+            // Utilisez RotateTowards pour ajuster progressivement chaque composante de la rotation vers 0
+            float rotationSpeed = 5f; // Ajustez cette valeur selon vos besoins
+            mancheRotation.transform.rotation = Quaternion.RotateTowards(rotationActuelle, rotationCible * Quaternion.Euler(rotationVaisseauActuelle), rotationSpeed * Time.deltaTime);
         }
+
         // Rotation du vaisseau avec les touches horizontales
         transform.Rotate(0f, deplacementHorizontal * vitesseRotation * Time.deltaTime, 0f);
 
